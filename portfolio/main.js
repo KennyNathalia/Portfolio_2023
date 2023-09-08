16a3bbdf46744a616c62633829389094bba0c972
Kenny Nathalia <99057679@mydavinci.nl>
2023-09-08 17:08:35 +0200

    added helpers and stars

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,5 +1,7 @@
 import './style.css'
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 
 //setup
 //scene and camera
@@ -33,10 +35,34 @@ pointLight.position.set(5,5,5)
 
 const ambientLight = new THREE.AmbientLight(0xffffff)
 
-// const lightHelper = new THREE.PointLightHelper(pointLight)
-// scene.add(lightHelper)
-
 scene.add(pointLight, ambientLight)
+
+//helpers
+//shows where the light is shining from
+const lightHelper = new THREE.PointLightHelper(pointLight)
+//displays grid
+const gridHelper = new THREE.GridHelper(200, 50)
+scene.add(lightHelper, gridHelper)
+
+//controls so that I can move the camera around
+const controls = new OrbitControls(camera, renderer.domElement);
+
+//adds stars
+function addStar(){
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff})
+  const star = new THREE.Mesh( geometry, material);
+
+  //randomly positions the xyz for each star. Chooses between -100 and 100
+  const [x, y, z] =  Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  //set position to the star and add it to the scene
+  star.position.set(x, y, z);
+  scene.add(star)
+}
+
+//array of 200. For each value in the array, call addStar function
+Array(200).fill().forEach(addStar)
 
 //animate function so I don't have to copy and paste the same renderer function again and again
 function animate(){
@@ -45,6 +71,9 @@ function animate(){
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
+
+  //updates control movements
+  controls.update();
 
   renderer.render(scene, camera);
 } 
